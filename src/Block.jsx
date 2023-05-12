6d09a795e90a58b4a349238bcf3b314d3fe67d73
Djamalutin <fisher.gerryy@mail.ru>
2023-05-12 16:43:37 +0300

    feature: creating map(cur => (<li>))

--- src/Block.jsx
+++ src/Block.jsx
@@ -3,7 +3,13 @@ export const Block = ({ value, currency, onChangeValue, onChangeCurrency }) => {
   return (
     <div className="block">
       <ul className="currencies">
-        {defaultCurrencies}
+        {defaultCurrencies.map((cur) => {
+          return (
+            <li key={cur} className={currency === cur ? "active" : ""}>
+              {cur}
+            </li>
+          );
+        })}
         <li>
           <svg height="50px" viewBox="0 0 50 50" width="50px">
             <rect fill="none" height="50" width="50"></rect>
