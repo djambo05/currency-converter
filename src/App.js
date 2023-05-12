14594d1dba2fb9bd2a22963c57bc9f7cc5246f89
Djamalutin <fisher.gerryy@mail.ru>
2023-05-12 17:26:37 +0300

    feature: useState and functions onChange

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
+import { useEffect, useState } from "react";
 import { Block } from "./Block";
 import "./index.scss";
 
 function App() {
+  const [fromCurrency, setFromCurrency] = useState("RUB");
+  const [toCurrency, setToCurrency] = useState("USD");
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(0);
+  const [rates, setRates] = useState({});
+
+  const onChangeFromPrice = (value) => {
+    setFromPrice(value);
+  };
+
+  const onChangeToPrice = (value) => {
+    setToPrice(value);
+  };
+
+  // useEffect(() => {
+  //   fetch("https://cdn.cur.su/api/latest.json")
+  //     .then((res) => res.json())
+  //     .then((json) => {
+  //       setRates(json.rates);
+  //       console.log(json.rates);
+  //     })
+  //     .catch((err) => {
+  //       console.warn(err);
+  //       alert("Не удалось получить информацию");
+  //     });
+  // });
   return (
     <div className="App">
       <Block
-        value={0}
-        currency="RUB"
-        onChangeCurrency={(cur) => console.log(cur)}
-      ></Block>
-      <Block value={0} currency="USD"></Block>
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeValue={onChangeFromPrice}
+        onChangeCurrency={setFromCurrency}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeValue={onChangeToPrice}
+        onChangeCurrency={setToCurrency}
+      />
     </div>
   );
 }
