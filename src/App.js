44d0d9c2cf5d2954ce20dcecceb78ac08ebc85d6
Djamalutin <fisher.gerryy@mail.ru>
2023-05-12 16:53:08 +0300

    feature: Block props

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import "./index.scss";
 function App() {
   return (
     <div className="App">
-      <Block></Block>
-      <Block></Block>
+      <Block
+        value={0}
+        currency="RUB"
+        onChangeCurrency={(cur) => console.log(cur)}
+      ></Block>
+      <Block value={0} currency="USD"></Block>
     </div>
   );
 }
