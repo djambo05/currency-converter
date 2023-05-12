2a8a66670d56852dd70da436b1793e194a319a82
Djamalutin <fisher.gerryy@mail.ru>
2023-05-12 16:27:31 +0300

    feature: adding svg, li, input to Block component

--- src/Block.jsx
+++ src/Block.jsx
@@ -2,7 +2,16 @@ export const Block = ({ value, currency, onChangeValue, onChangeCurrency }) => {
   const defaultCurrencies = ["RUB", "USD", "EUR", "GBP"];
   return (
     <div className="block">
-      <ul className="currencies">{defaultCurrencies}</ul>
+      <ul className="currencies">
+        {defaultCurrencies}
+        <li>
+          <svg>
+            <rect></rect>
+            <polygon></polygon>
+          </svg>
+        </li>
+      </ul>
+      <input type="number" placeholder={0}></input>
     </div>
   );
 };
