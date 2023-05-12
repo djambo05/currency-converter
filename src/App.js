50a6908a154a16fcf36ba7c6575128bf808d910c
Djamalutin <fisher.gerryy@mail.ru>
2023-05-12 16:22:51 +0300

    feature: building the application architecture

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Block></Block>
+      <Block></Block>
+    </div>
+  );
 }
 
 export default App;
