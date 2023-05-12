4495639be98eb4e7af600e6dd60579e55cae465f
Djamalutin <fisher.gerryy@mail.ru>
2023-05-12 16:41:05 +0300

    feature: svg settings

--- src/Block.jsx
+++ src/Block.jsx
@@ -5,9 +5,9 @@ export const Block = ({ value, currency, onChangeValue, onChangeCurrency }) => {
       <ul className="currencies">
         {defaultCurrencies}
         <li>
-          <svg>
-            <rect></rect>
-            <polygon></polygon>
+          <svg height="50px" viewBox="0 0 50 50" width="50px">
+            <rect fill="none" height="50" width="50"></rect>
+            <polygon points="47.25,15 45.164,12.914 25,33.078 4.836,12.914 2.75,15 25,37.25 "></polygon>
           </svg>
         </li>
       </ul>
